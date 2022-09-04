b6d5167e7b271fe1f783360ccb36ed66fc0df242
fantasticsoul <zhongzhengkai@gmail.com>
2022-09-04 22:30:10 +0800

    feat:bump hel packs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 async function main() {
-  const { libReady } = await import('hel-lib-proxy');
+  const { libReady, isMasterApp } = await import('hel-lib-proxy');
   const { LIB_NAME } = await import('./configs/subApp');
   // 如有其他远程包依赖并且需要在内部使用静态导入的语法，可使用 preFetchLib 来加载这些包体
   // const { preFetchLib } = await import('hel-micro');
@@ -10,6 +10,12 @@ async function main() {
   const libProperties = await import('./entrance/libProperties');
   // 注意此处传递的是 default
   libReady(LIB_NAME, libProperties.default);
+
+  // 本地启动自己载入自己时，走的特殊逻辑
+  // 仅用于演示，可删除或者根据自己想在本地启动时做一些其他工作而调整
+  if (isMasterApp()) {
+    await import('./loadApp');
+  }
 };
 
 main().catch(console.error);
