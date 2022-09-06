ba45e4e071f24258546a47ffb857b4f8c23cc1e8
fantasticsoul <zhongzhengkai@gmail.com>
2022-09-06 12:22:30 +0800

    feat:mod cust

--- scripts/meta.js
+++ scripts/meta.js
@@ -9,6 +9,8 @@ const path = require('path');
 const helDevUtils = require('hel-dev-utils');
 const packageJson = require('../package.json');
 
+console.log('HEL_APP_HOME_PAGE is: ', process.env.HEL_APP_HOME_PAGE);
+
 helDevUtils.extractHelMetaJson({
   /** 形如 https://xxxxx.yy.com/<zoneName>/<relativeDirName> */
   buildDirFullPath: path.join(__dirname, '../hel_dist'),
