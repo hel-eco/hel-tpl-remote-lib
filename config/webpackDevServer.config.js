78e8b2773a37338643e02b9aebfa83b64a727824
fantasticsoul <zhongzhengkai@gmail.com>
2022-10-02 12:13:45 +0800

    feat:mod cos config

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -127,9 +127,9 @@ module.exports = function (proxy, allowedHost) {
       app.use(noopServiceWorkerMiddleware(paths.publicUrlOrPath));
     },
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-    }
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': '*',
+      'Access-Control-Allow-Headers': '*',
+    },
   };
 };
