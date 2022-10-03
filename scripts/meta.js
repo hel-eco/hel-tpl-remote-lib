969100c4cc5b1c4317b97957e7e8c85918d8c9f3
fantasticsoul <zhongzhengkai@gmail.com>
2022-10-03 21:34:50 +0800

    faet:优化脚本

--- scripts/meta.js
+++ scripts/meta.js
@@ -8,10 +8,12 @@
 const path = require('path');
 const helDevUtils = require('hel-dev-utils');
 const packageJson = require('../package.json');
+const subApp = require('../config/subApp');
 
-console.log('HEL_APP_HOME_PAGE is: ', process.env.HEL_APP_HOME_PAGE);
-
+// npm run build_for_cust 设定的 HEL_APP_HOME_PAGE 值指向本地 http-server 静态服务 host
+// 该设定会让构建产物时生成的 publicPathOrUrl 优先使用此值，而非 subApp.getPublicPathOrUrl('http://localhost:3000/') 传递的值
 helDevUtils.extractHelMetaJson({
+  appHomePage: subApp.homePage,
   /** 形如 https://xxxxx.yy.com/<zoneName>/<relativeDirName> */
   buildDirFullPath: path.join(__dirname, '../hel_dist'),
   packageJson,
