d4f1bbee7b934c9c22d47c1fd768cfff813b926f
刘亚辉 <30796346+lyh0371@users.noreply.github.com>
2022-10-12 14:46:50 +0800

    feat:添加prepublishOnly钩子

---
+++ scripts/prepublishOnly.js
@@ -0,0 +1,26 @@
+/*
+|--------------------------------------------------------------------------
+| npm publush 前检查根目录下 package.json 的版本号是否和 hel_dist/hel-meta.json 一致
+|--------------------------------------------------------------------------
+*/
+
+const chalk = require("react-dev-utils/chalk");
+const pkg = require("../package.json");
+const fs = require("fs");
+
+const existHelMeata = fs.existsSync("../hel_dist/hel-meta.json");
+
+// 检查是否执行npm run build 打包命令
+if (!existHelMeata) {
+  console.log(chalk.red("Run the 'npm run build' command first.\n"));
+  process.exit(1);
+}
+// 检查根目录下 package.json 的版本号是否和 hel_dist/hel-meta.json 一致
+if (helMeta.app.build_version !== pkg.version) {
+  console.log(
+    chalk.red(
+      "The package.json version number and the hel_dist/hel-meta.json version number must be the same.\n"
+    )
+  );
+  process.exit(1);
+}
