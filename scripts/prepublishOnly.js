067e64d64e01e812031cd083f25e3557acb973c9
刘亚辉 <30796346+lyh0371@users.noreply.github.com>
2022-10-16 12:45:49 +0800

    feat:新增prepublishOnly钩子函数

--- scripts/prepublishOnly.js
+++ scripts/prepublishOnly.js
@@ -8,16 +8,17 @@ const chalk = require("react-dev-utils/chalk");
 const pkg = require("../package.json");
 const fs = require("fs");
 const path = require("path");
-const helMeta = require("../hel_dist/hel-meta.json");
-const existHelMeata = fs.existsSync(path.resolve(__dirname), "../hel_dist");
+const helDistPath = path.resolve(__dirname, "../hel_dist/hel-meta.json");
+const existHelMeata = fs.existsSync(helDistPath);
 
 // 检查是否执行npm run build 打包命令
 if (!existHelMeata) {
   console.log(chalk.red("Run the 'npm run build' command first.\n"));
   process.exit(1);
 }
+const { app } = JSON.parse(fs.readFileSync(helDistPath, "utf-8"));
 // 检查根目录下 package.json 的版本号是否和 hel_dist/hel-meta.json 一致
-if (helMeta.app.build_version !== pkg.version) {
+if (app?.build_version !== pkg.version) {
   console.log(
     chalk.red(
       "The package.json version number and the hel_dist/hel-meta.json version number must be the same.\n"
