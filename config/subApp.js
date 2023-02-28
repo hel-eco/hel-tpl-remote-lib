50824a167d7ce45297849f61b7a9dcaf7809295e
fantasticsoul <zhongzhengkai@gmail.com>
2023-02-28 09:22:26 +0800

    feat: add comma

--- config/subApp.js
+++ config/subApp.js
@@ -3,4 +3,10 @@ const pkg = require('../package.json');
 
 const subApp = helDevUtils.createLibSubApp(pkg, { npmCdnType: 'unpkg' });
 
+// 自定义 homePage，形如：https://youhost.com/aa/bb、 /aa/bb、../aa/bb
+// const subApp = helDevUtils.createLibSubApp(pkg, { npmCdnType: 'unpkg', homePage: './xx' });
+
+// 自定义 homePage，不拼接名字和版本号
+// const subApp = helDevUtils.createLibSubApp(pkg, { npmCdnType: 'unpkg', homePage: './xx', handleHomePage: false });
+
 module.exports = subApp;
