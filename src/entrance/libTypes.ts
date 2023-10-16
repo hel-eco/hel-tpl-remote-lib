5cb0e9577738af3ae1d109ad57ba34ce3e21f3e1
fantasticsoul <zhongzhengkai@gmail.com>
2023-10-16 19:36:59 +0800

    feat: export detail method

--- src/entrance/libTypes.ts
+++ src/entrance/libTypes.ts
@@ -5,6 +5,13 @@ import { LIB_NAME } from 'configs/subApp';
 
 export const lib = exposeLib<LibProperties>(LIB_NAME);
 
+// suport writing: import { regs, num, myMod } from 'hel-tpl-remote-lib';
+export const {
+  regs,
+  num,
+  myMod,
+} = lib;
+
 export type Lib = LibProperties;
 
 export default lib;
